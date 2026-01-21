479a060ad0ff4fd17b0292a120327292e36f64da
mega-mix <mega-mix@web.de>
2026-01-21 12:47:34 +0100

    Add files via upload

--- app/js/main.js
+++ app/js/main.js
@@ -1,26 +1,28 @@
 import SaveGame from "./models/SaveGame.js";
 
-let user = netlifyIdentity.currentUser();
-const isLocal = window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1";
-
-if (isLocal && !user) {
-    console.warn("⚠️ Entwicklungsumgebung erkannt: Fake-Admin aktiviert!");
+window.netlifyIdentity.on("init", user => {
     
-    user = {
-        email: "entwickler@localhost",
-        user_metadata: { 
-            full_name: "Super-Entwickler (Lokal)" 
-        },
-        app_metadata: { 
-            roles: ["admin", "user"]
-        }
-    };
-}
+    const isLocal = window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1";
 
-if (!user) {
-    window.location.href = "/"; // Zurück zum Login
-} else {
-    // A. Benutzernamen & E-Mail auslesen
+
+    if (isLocal && !user) {
+        console.warn("⚠️ Entwicklungsumgebung erkannt: Lokal-Admin aktiviert!");
+        user = {
+            email: "admin@localhost",
+            user_metadata: { 
+                full_name: "Admin (Lokal)" 
+            },
+            app_metadata: { 
+                roles: ["admin", "user"]
+            }
+        };
+    }
+
+    if (!user) {
+        window.location.href = "/"; 
+        return;
+    }
+
     const email = user.email;
     const fullName = user.user_metadata.full_name || "Unbekannt";
     
@@ -29,29 +31,37 @@ if (!user) {
     const roles = user.app_metadata.roles || [];
     const isAdmin = roles.includes("admin");
 
-    // Admin-Funktionen freischalten
     if (isAdmin) {
         console.log("Admin-Rechte erkannt.");
     } else {
         console.log("Nur User-Rechte.");
     }
+
+    // LOGIK ERST AB HIER!
+    setupUI();
+});
+
+
+function setupUI() {
+    const output = document.getElementById("output");
+    const input = document.getElementById("input");
+    const btnLaden = document.getElementById("btnLaden");
+    const btnSpeichern = document.getElementById("btnSpeichern");
+
+    btnLaden.addEventListener('click', e => {
+        // Laden Logik
+    });
+
+    btnSpeichern.addEventListener('click', e => {
+        const playerName = input.value;
+        const save = new SaveGame(playerName, 1);
+        console.log("Save erstellt:", save.player);
+    });
 }
 
-const output = document.getElementById("output");
-const input = document.getElementById("input");
-const btnLaden = document.getElementById("btnLaden");
-const btnSpeichern = document.getElementById("btnSpeichern");
 
-btnLaden.addEventListener('click', e => {
+if (window.netlifyIdentity.currentUser()) {
 
-});
+}
 
-btnSpeichern.addEventListener('click', e => {
-    const playerName = input.value;
-    const save = new SaveGame(playerName, 1);
-    console.log("Save erstellt:");
-    console.log(save.player);
-
-
-
-});
+window.netlifyIdentity.init();
